5529e08e9d473c6c1974120c007754a979227361
sauravkr317 <sauravkr317@gmail.com>
2021-05-19 21:45:19 +0530

    second changes

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,14 @@ console.log("hello")
 
 let navlist = document.querySelector(".navlist");
 let hamburger = document.querySelector(".hamburger");
+let close = document.querySelector(".close");
 hamburger.addEventListener("click", () =>{
-    navlist.classList.toggle("show");
+    navlist.classList.add("show");
+    hamburger.style.display = "none";
+    close.style.display = "block";
+    close.addEventListener("click", () =>{
+        navlist.classList.remove("show");
+        hamburger.style.display = "block";
+        close.style.display = "none";
+    })
 })
