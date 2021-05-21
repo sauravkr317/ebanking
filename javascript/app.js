86f1cdf43727efc371c5a395f221f28efeaa50c2
sauravkr317 <sauravkr317@gmail.com>
2021-05-21 12:49:27 +0530

    third changes

--- javascript/app.js
+++ javascript/app.js
@@ -3,11 +3,13 @@ console.log("hello")
 let navlist = document.querySelector(".navlist");
 let hamburger = document.querySelector(".hamburger");
 let close = document.querySelector(".close");
-hamburger.addEventListener("click", () =>{
+
+
+hamburger.addEventListener("click", () => {
     navlist.classList.add("show");
     hamburger.style.display = "none";
     close.style.display = "block";
-    close.addEventListener("click", () =>{
+    close.addEventListener("click", () => {
         navlist.classList.remove("show");
         hamburger.style.display = "block";
         close.style.display = "none";
